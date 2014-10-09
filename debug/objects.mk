fde66e06b1aa30723972b375cb1a6d0ad533ef92
Dominic Marcello <dmarce1@tigers.lsu.edu>
2014-10-09 07:51:13 -0500

    added build dirs

---
+++ debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lsilo -lboost_context -lboost_atomic -ltcmalloc_minimal -lhwloc -lhpxd -lhpx_initd -lhpx_serializationd -ldl -lrt -lboost_program_options -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_regex -lboost_serialization -lboost_system -lboost_thread
+
---
+++ release/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/load_balancer.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/load_balancer.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/load_balancer.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	mpic++ -DHPX_APPLICATION_EXPORTS -DHPX_ENABLE_ASSERT_HANDLER -I/home/dmarce1/include/hpx/external -O3 -c  -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
